0137ac18d51247c3fbade56dab69fb448e816829
qamichaelmaia <contatomichaelmaia@gmail.com>
2024-03-29 18:41:45 -0300

    finalizando tarefa

--- cypress/e2e/criandoContaPageObjects.cy.js
+++ cypress/e2e/criandoContaPageObjects.cy.js
@@ -2,25 +2,26 @@
 
 const { homePage } = require("../support/pages/home.page");
 const signPage = require("../support/pages/sign.page");
-const { nome, sobrenome, telefone, email, password, reenterPsw, } = require("../fixtures/data.json");
-const profilePage  = require("../support/pages/profile.page");
+const {nome,sobrenome,telefone,password,reenterPsw,} = require("../fixtures/data.json");
+const profilePage = require("../support/pages/profile.page");
 
 describe("Fluxo de criação de conta", () => {
+
     beforeEach(() => {
-        cy.setCookie("ebacStoreVersion", "v2", {domain: "lojaebac.ebaconline.art.br",});
+        cy.setCookie("ebacStoreVersion", "v2", {
+            domain: "lojaebac.ebaconline.art.br",
+        });
         cy.visit("/");
     });
+
     it("Deve criar conta com sucesso", () => {
-        
-        let email = `michael@ebac${Math.floor(Math.random() * 100000000)}.com.br`; // Gera um email aleatório
+        let email = `michael${Math.floor(Math.random() * 100000000)}@ebac.com`; // Gera um email aleatório
 
         homePage.openMenu("Account");
         cy.get('[data-testid="signUp"]').click();
 
-        signPage.sign(nome, sobrenome, telefone, email, password, reenterPsw); //informações buscadas em (fixure/data.json) alterar e-mail pois está sem faker
-
-        cy.get('[href="/Tab/Account"]').click()
-        profilePage.customerName().should('have.text', 'Maia Michael')
-
+        signPage.sign(nome, sobrenome, telefone, email, password, reenterPsw);
+        cy.get('[href="/Tab/Account"]').click();
+        profilePage.customerName().should("have.text", "Maia Michael");
     });
 });
